0cc717a3389c4a0a706cbf1617a143550ec5b4d5
thompson318 <s.thompson@ucl.ac.uk>
2023-08-29 14:15:15 +0000

    Deploying to gh-pages from @ thompson318/dashboard_for_magpi@7182f4086f7f84512bd95415d6bc5c482568e446 🚀

---
+++ get_github_repos.py
@@ -0,0 +1,23 @@
+from github import Github
+from github.GithubException import RateLimitExceededException
+
+from sksurgerystats.common import add_github_package
+from sksurgerystats.from_github import get_token
+
+token = None
+token = get_token()
+
+if token is not None:
+    g = Github(token)
+    reps = g.search_repositories(
+        query="{} in:name".format("magpie-pkg")
+    )
+
+    try:
+        for rep in reps:
+            print (rep)
+            add_github_package(rep)
+    except RateLimitExceededException:
+        print("Got a rate limit exception from Github, probably because ",
+            "your search term returned too many matches. ",
+            "I've halted adding new libraries from GitHub")
